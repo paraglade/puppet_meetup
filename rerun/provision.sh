d01f613869a4a3747c573863b2472b8a4a4591f1
Nelson Roberts <paraglade@deviantart.com>
2016-05-27 12:59:43 -0700

    fixup vagrant and provisioning

--- rerun/provision.sh
+++ rerun/provision.sh
@@ -1,22 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 
-function run_step {
+apt-get update
+apt-get upgrade -y
+apt-get install -y --force-yes git zip autoconf discount python-pygments vim dh-make build-essential devscripts
+apt-get autoremove -y
 
-  cmd=$*
-  cmd_hash=$(echo ${cmd} | md5sum | cut -d " " -f 1)
-  cmd_hash_file="/tmp/.step-${cmd_hash}"
-  [ ! -e ${cmd_hash_file} ] && ${cmd} && touch ${cmd_hash_file}
-}
+git clone https://github.com/rerun/rerun.git
 
-run_step apt-get update
-run_step apt-get upgrade -y
-run_step apt-get install -y git zip autoconf discount python-pygments
-run_step apt-get autoremove -y
+cp rerun/etc/bash_completion.sh /etc/bash_completion.d/
+cp /vagrant/rerun_profile.sh /etc/profile.d
 
-run_step git clone https://github.com/rerun/rerun.git
-
-run_step cp rerun/rerun /usr/bin
-run_step cp rerun/etc/bash_completion.sh /etc/bash_completion.d/rerun
-run_step mkdir -p /var/rerun
-run_step cp -a rerun/modules /var/rerun/modules
-run_step rm -rf rerun
+chown -R vagrant:vagrant rerun
--- rerun/rerun_profile.sh
+++ rerun/rerun_profile.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-export RERUN_MODULES=${HOME}/rerun/modules:/var/rerun/modules
+export RERUN_MODULES=${HOME}/rerun/modules
 export RERUN_COLOR=true
 mkdir -p ${HOME}/rerun/modules
+export PATH=${PATH}:${HOME}/rerun
